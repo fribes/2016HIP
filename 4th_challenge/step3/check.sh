4d8b96114299af9b70e78503d856b38cd579aa00
Fabien RIBES <fabien.ribes@legrand.fr>
2016-04-11 14:39:22 +0200

    Done

--- 4th_challenge/step3/check.sh
+++ 4th_challenge/step3/check.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-known="HIP{f3a306095ecc4dad3d0056ad7b0c135afd89e3127e2"
-len=${#known}
-
-for j in `cat chars.txt`
+#known="HIP{f3a306095ecc4dad3d0056ad7b0c135afd89e3127e2"
+known="HIP{"
+while true
 do
-	for i in $j
+	len=${#known}
+	found="no"
+	for j in `cat chars.txt`
 	do
-		echo $known$i | strace ./chall.bin 2>&1 | grep -A$((1+$len)) read\(0 | head -$((2+$len)) | tail -1 | grep -v "write(1, \"A\", 1A)"
-		if [ $? == 0 ]
+		for i in $j
+		do
+			echo $known$i | strace ./chall.bin 2>&1 | grep -A$((1+$len)) read\(0 | head -$((2+$len)) | tail -1 | grep -v "write(1, \"A\", 1A)" >/dev/null
+			if [ $? == 0 ]
+			then
+				known=$known$i
+				echo  $known
+				found="yes"
+				break	
+			fi
+		done
+		if [ $found = "yes" ]
 		then
-			echo Found !!! $i
-			exit
+			break
 		fi
 	done
 done
